b7bd5bb6cece3fae9c2d9bb74d6fc37366f6db9e
IGNIT3-xD <md.imranali089@gmail.com>
2025-08-04 09:40:29 +0600

    some changes

--- Problem-04.js
+++ Problem-04.js
@@ -2,7 +2,15 @@
 var fileName = "pdfData.jpg";
 //write your code here
 
-if (fileName[0].includes('#') || fileName.endsWith('.pdf') || fileName.endsWith('.docx')) {
+if (fileName[0].includes('#') || fileName.slice(-4) === '.pdf' || fileName.slice(-5) === ('.docx')) {
+    console.log('Store')
+}
+else {
+    console.log('Delete')
+}
+
+// Or
+if (fileName.startsWith('#') || fileName.endsWith('.pdf') || fileName.endsWith('.docx')) {
     console.log('Store')
 }
 else {
